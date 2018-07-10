143a1c748bdbf7ae343cd14050660dc91b81441b
Sephora <seph156@gmail.com>
2018-07-10 10:27:35 -0700

    second set of exercises

---
+++ dml_ddl_dcl.sql
@@ -0,0 +1,28 @@
+/*
+  1. DDL - ALTER, DROP, CREATE
+     DML - SELECT, UPDATE, INSERT, DELETE
+
+  2. DML because it is related to the manipulation of data and not the schema
+
+  3. DDL because it is part of the schema and is used to define what kind of
+     data should be used and what columns should be in the new table.
+
+  4. DDL. This statement does not manipulate the data, rather it alters the
+    table itself.
+
+  5. DML. INSERT is a statement that manipulates data and not how the data is
+     structured.
+
+  6. DML. This statement manipulates data and doesn't affect how the data is
+     structured.
+
+  7. \d is a meta command, not an sql statement.
+
+  8. DML. This statement manipulates data and does not manipulate the schema
+
+  9. DDL. Although any data within the database would be lost if this statement
+     is executed, so we could say that data is being manipulated as well, so
+     it's also DML. All other DROP statements are considered DDL.
+
+  10. DDL. CREATE statements are part of Data Definition Language
+*/
